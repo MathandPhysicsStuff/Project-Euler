908f21ee389f64328abb6f83b145e9da77b0997b
MathandPhysicsStuff <emailforgithub51@protonmail.com>
2024-04-16 16:30:15 -0400

    solved problem017 in c

--- c/problem017.c
+++ c/problem017.c
@@ -10,5 +10,107 @@ contains 20 letters.
 The use of "and" when writing out numbers is in compliance with British usage.
 */
 #include <stdio.h>
+#include <string.h>
+
+/*
+def numberToWords(number):
+    under20 = ["", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+               "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"]
+    tens = ["", "ten", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"]
+
+    if number == 1000:
+        words = "onethousand"
+        return words
+    
+    elif 0 < number < 20:
+        words = under20[number]
+        return words
+
+    elif 19 < number < 100:
+        words = tens[number // 10] + under20[number % 10]
+        return words
+
+    elif 99 < number < 1000:
+        words = under20[number // 100] + "hundred"
+        if 0 < number % 100 < 20:
+            words += "and" + under20[number % 20]
+        elif 19 < number % 100 < 100:
+            words += "and" + tens[(number % 100) // 10] + under20[number % 10]
+        return words
+
+    else:
+        words = ""
+        return words
+*/
+
+void number_to_words(int number, char *words)
+{
+    char under20[][10] = {"", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+                          "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"};
+    char tens[][10] = {"", "ten", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"};
+        
+
+    if (number == 1000)
+        strcpy(words, "onethousand");
+ 
+    else if (0 < number && number < 20)
+        strcpy(words, under20[number]);
+
+    else if (19 < number && number < 100)
+    {
+        strcpy(words, tens[number / 10]);
+        strcat(words, under20[number % 10]);
+    }
+
+    else if (99 < number && number < 1000)
+    {
+        strcpy(words, under20[number / 100]);
+        strcat(words, "hundred");
+
+        if (0 < number % 100 && number % 100 < 20)
+        {
+            strcat(words, "and");
+            strcat(words, under20[number % 20]);
+        }
+        else if (19 < number % 100 && number % 100 < 100)
+        {
+            strcat(words, "and");
+            strcat(words, tens[(number % 100) / 10]);
+            strcat(words, under20[number % 10]);
+        }
+    }
+
+    else
+        strcpy(words, "");
+}
+
+int main()
+{
+    char words[100];
+    int sum = 0;
+
+    for (int i = 1; i <= 1000; i++)
+    {
+        number_to_words(i, words);
+        sum += strlen(words);
+    }
+    
+    printf("Number Letter Counts\n");
+    printf("Problem 17\n\n");
+
+    printf("Total number of characters: %d\n", sum);
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
 
 
